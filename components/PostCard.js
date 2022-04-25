15e464f4836f87d2f871ef64c60f5a5f7d76003a
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-04-26 00:10:20 +0900

    댓글 달린 리스트 구현

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,14 +1,20 @@
 import React, { useCallback, useState } from 'react';
-import { Button, Card, Popover, Avatar } from 'antd';
+import { Button, Card, Popover, Avatar, List } from 'antd';
 import {
 	RetweetOutlined,
 	HeartOutlined,
 	MessageOutlined,
 	EllipsisOutlined,
 	HeartTwoTone,
+	MessageTwoTone,
 } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+	margin-bottom: 20px;
+`;
 
 const PostCard = ({ post }) => {
 	const { me } = useSelector((state) => state.user);
@@ -16,48 +22,76 @@ const PostCard = ({ post }) => {
 
 	const [liked, setLiked] = useState(false);
 	const likedToggle = useCallback(() => {
-		console.log(liked);
-
 		setLiked((prev) => !prev);
 	}, []);
 
+	const [commentFormOpened, setCommentFormOpened] = useState(false);
+	const commentToggle = useCallback(() => {
+		setCommentFormOpened((prev) => !prev);
+	}, []);
+
 	return (
-		<Card
-			// style={{ width: 300 }}
-			// cover={post.Images[0] && <PostImages images={post.Images} />}
-			actions={[
-				<RetweetOutlined key="retweet" />,
-				liked ? (
-					<HeartTwoTone twoToneColor="#eb2f96" onClick={likedToggle} />
-				) : (
-					<HeartOutlined key="heart" onClick={likedToggle} />
-				),
-				<MessageOutlined key="comment" />,
-				<Popover
-					key="more"
-					content={
-						<Button.Group>
-							{id && id === post.User.id ? (
-								<>
-									<Button>수정</Button>
-									<Button type="danger">삭제</Button>
-								</>
-							) : (
-								<Button>신고</Button>
-							)}
-						</Button.Group>
-					}
-				>
-					<EllipsisOutlined key="ellipsis" />
-				</Popover>,
-			]}
-		>
-			<Card.Meta
-				avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
-				title="Card title"
-				description="얍!"
-			/>
-		</Card>
+		<CardWrapper>
+			<Card
+				// style={{ width: 300 }}
+				// cover={post.Images[0] && <PostImages images={post.Images} />}
+				actions={[
+					<RetweetOutlined key="retweet" />,
+					liked ? (
+						<HeartTwoTone
+							key="heart"
+							twoToneColor="#eb2f96"
+							onClick={likedToggle}
+						/>
+					) : (
+						<HeartOutlined key="heart" onClick={likedToggle} />
+					),
+					<MessageOutlined key="comment" onClick={commentToggle} />,
+					<Popover
+						key="more"
+						content={
+							<Button.Group>
+								{id && id === post.User.id ? (
+									<>
+										<Button>수정</Button>
+										<Button type="danger">삭제</Button>
+									</>
+								) : (
+									<Button>신고</Button>
+								)}
+							</Button.Group>
+						}
+					>
+						<EllipsisOutlined key="ellipsis" />
+					</Popover>,
+				]}
+			>
+				<Card.Meta
+					avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+					title="Card title"
+					description="얍!"
+				/>
+			</Card>
+			{commentFormOpened && (
+				<>
+					{/* <CommentForm /> */}
+					{/* 댓글리스트 */}
+					<List
+						itemLayout="horizontal"
+						dataSource={post.Comments}
+						renderItem={(item) => (
+							<List.Item>
+								<List.Item.Meta
+									avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+									title={item.User.nickname}
+									description={item.content}
+								/>
+							</List.Item>
+						)}
+					/>
+				</>
+			)}
+		</CardWrapper>
 	);
 };
 
