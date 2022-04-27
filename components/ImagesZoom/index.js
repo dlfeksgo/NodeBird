d1949276777c21225562100ad24aacc8e570e6dd
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-04-27 23:57:59 +0900

    해시태그

--- components/ImagesZoom/index.js
+++ components/ImagesZoom/index.js
@@ -1,83 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import styled, { createGlobalStyle } from 'styled-components';
-import { CloseOutlined } from '@ant-design/icons';
 
-const Global = createGlobalStyle`
-  .slick-slide {
-    display: inline-block;
-  }
-  .ant-card-cover {
-    transform: none !important;
-  }
-`;
-
-const Overlay = styled.div`
-	position: fixed;
-	z-index: 5000;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-`;
-
-const Header = styled.header`
-	height: 44px;
-	background: white;
-	position: relative;
-	padding: 0;
-	text-align: center;
-
-	& h1 {
-		margin: 0;
-		font-size: 17px;
-		color: #333;
-		line-height: 44px;
-	}
-`;
-
-const SlickWrapper = styled.div`
-	height: calc(100% - 30px);
-	background: #090909;
-`;
-
-const CloseBtn = styled(CloseOutlined)`
-	position: absolute;
-	right: 0;
-	top: 0;
-	padding: 15px;
-	line-height: 14px;
-	cursor: pointer;
-`;
-
-const Indicator = styled.div`
-	text-align: center;
-
-	& > div {
-		width: 75px;
-		height: 30px;
-		line-height: 30px;
-		border-radius: 15px;
-		background: #313131;
-		display: inline-block;
-		text-align: center;
-		color: white;
-		font-size: 15px;
-	}
-`;
-
-const ImgWrapper = styled.div`
-	padding: 32px;
-	text-align: center;
-
-	& img {
-		margin: 0 auto;
-		max-height: 750px;
-	}
-`;
+import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper } from './styles';
 
 const ImagesZoom = ({ images, onClose }) => {
 	const settings = {
@@ -115,6 +40,7 @@ const ImagesZoom = ({ images, onClose }) => {
 
 ImagesZoom.propTypes = {
 	images: PropTypes.arrayOf(PropTypes.any),
+	onClose: PropTypes.func.isRequired,
 };
 
 export default ImagesZoom;
--- components/PostCard.js
+++ components/PostCard.js
@@ -14,6 +14,7 @@ import styled from 'styled-components';
 
 import CommentForm from './CommentForm';
 import PostImages from './PostImages';
+import PostCardContent from './PostCardContent';
 
 const CardWrapper = styled.div`
 	margin-bottom: 20px;
@@ -78,7 +79,7 @@ const PostCard = ({ post }) => {
 				<Card.Meta
 					avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
 					title="Card title"
-					description="첫 번째 게시글 #해시태그 #익스프레스"
+					description={<PostCardContent postData={post.content} />}
 				/>
 			</Card>
 			{commentFormOpened && (
