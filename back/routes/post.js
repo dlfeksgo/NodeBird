46b145c23b0707c879bf8152aadcb66786a00ac3
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-05-22 23:58:32 +0900

    Merge remote-tracking branch 'origin/main'

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//router.post('/post', (req, res) => {
+//router.post('/post', (req, res) => {dsfd
 router.post('/', (req, res) => {
 	res.json({
 		id: 1,
