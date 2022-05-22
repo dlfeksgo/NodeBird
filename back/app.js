262ebd714e4bf9d356b90e0b20a6c418eb4fe2e1
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-05-23 02:52:32 +0900

    bcrypt와 비밀번호 암호화

--- back/app.js
+++ back/app.js
@@ -1,12 +1,3 @@
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-// 	req.end('Hello node');
-// });
-
-// server.listen(3065, () => {
-// 	console.log('서버 실행 중');
-// });
-
 const express = require('express');
 const db = require('./models');
 const app = express();
--- back/routes/user.js
+++ back/routes/user.js
@@ -3,14 +3,26 @@ const { User } = require('../models/user');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-	// POST /user/마지막테스트
-	await User.create({
-		email: req.body.email,
-		nickname: req.body.nickname,
-		password: req.body.password,
-	});
-	res.send('하이');
+// POST /user
+router.post('/', async (req, res, next) => {
+	try {
+		const exUser = User.findOne({
+			where: { email: req.body.email },
+		});
+		if (exUser) {
+			return res.status(403).send('이미 등록된 이메일입니다.');
+		}
+		const hashedPassword = await bcrypt.hash(req.body.password, 12);
+		await User.create({
+			email: req.body.email,
+			nickname: req.body.nickname,
+			password: hashedPassword,
+		});
+		res.send('하이');
+	} catch (err) {
+		console.log(err);
+		next(error);
+	}
 });
 
 module.exports = router;
